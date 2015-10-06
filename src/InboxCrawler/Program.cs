3009f646db66b780cd8ed66015a43f66d96a9abe
Steve Butler <stephbu@gmail.com>
2015-10-06 10:29:47 -0700

    More cleanup

--- src/InboxCrawler/Program.cs
+++ src/InboxCrawler/Program.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 
 namespace InboxCrawler
 {
-	class MainClass
+    class MainClass
 	{
 		static JsonSerializerSettings settings = new JsonSerializerSettings {
 			DateFormatHandling = DateFormatHandling.IsoDateFormat,
