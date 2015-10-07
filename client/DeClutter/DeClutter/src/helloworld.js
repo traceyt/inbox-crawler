c4fe7baf8de920e9b1d0ffa144beade16450d030
Tracey Trewin <tracey@trewin.com>
2015-10-07 10:52:12 -0700

    add a runtime component to be called from the client

---
+++ client/DeClutter/DeClutter/src/helloworld.js
@@ -0,0 +1,4 @@
+// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+React.render(React.createElement("h1", null, "Hello, world!"), document.getElementById('example'));
