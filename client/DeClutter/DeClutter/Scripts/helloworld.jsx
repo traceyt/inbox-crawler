2c8f40d640b0bf31ef0319ecebca1d7cb784c7c4
Tracey Trewin <tracey@trewin.com>
2015-10-08 09:15:06 -0700

    add a new C# client

--- client/DeClutter/DeClutter/Scripts/helloworld.jsx
+++
@@ -1,5 +0,0 @@
-// A '.tsx' file enables JSX support in the TypeScript compiler, 
-// for more information see the following page on the TypeScript wiki:
-// https://github.com/Microsoft/TypeScript/wiki/JSX
-/// <reference path="../typing/react.d.ts" />
-React.renderComponent(<h1>Hello, world!</h1>, document.getElementById('content'));
